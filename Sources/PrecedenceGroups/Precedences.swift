112c52052d3ee5ecb6be1474306c198149e970d2
Michael Arrington <29713320+SleekDiamond41@users.noreply.github.com>
2022-10-27 22:16:06 -0600

    added handling for range precedence

--- Sources/PrecedenceGroups/Precedences.swift
+++ Sources/PrecedenceGroups/Precedences.swift
@@ -4,50 +4,59 @@ import Foundation
 precedencegroup SingleTypeComposition {
 	associativity: left
 	higherThan: EffectfulComposition
+	lowerThan: RangeFormationPrecedence
 }
 
 // >=>
 precedencegroup EffectfulComposition {
 	associativity: left
 	higherThan: BackwardComposition
+	lowerThan: RangeFormationPrecedence
 }
 
 // <<<
 precedencegroup BackwardComposition {
 	associativity: left
 	higherThan: ForwardComposition
+	lowerThan: RangeFormationPrecedence
 }
 
 // >>>
 precedencegroup ForwardComposition {
 	associativity: left
 	higherThan: ToInoutComposition
+	lowerThan: RangeFormationPrecedence
 }
 
 // <>>
 precedencegroup FromInoutComposition {
 	higherThan: ToInoutComposition
+	lowerThan: RangeFormationPrecedence
 }
 
 // <<>
 precedencegroup ToInoutComposition {
 	higherThan: BackwardApplication
+	lowerThan: RangeFormationPrecedence
 }
 
 // |>>
 precedencegroup MutatingForwadApplication {
 	associativity: left
 	higherThan: BackwardApplication
+	lowerThan: RangeFormationPrecedence
 }
 
 // <|
 precedencegroup BackwardApplication {
 	associativity: left
 	higherThan: ForwardApplication
+	lowerThan: RangeFormationPrecedence
 }
 
 // |>
 precedencegroup ForwardApplication {
 	associativity: left
 	higherThan: AssignmentPrecedence
+	lowerThan: RangeFormationPrecedence
 }
